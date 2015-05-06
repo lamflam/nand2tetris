751616b2d4dad8f20b5b3a277301342108a20878
Kevin LaFlamme <laflamme.kevin@gmail.com>
2015-05-06 08:34:17 -0400

    Fix buffer overrun in screen fill program

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,4 +8,55 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+@SCREEN
+D=A
+@8192
+D=D+A
+@end
+M=D
+@RESET
+0;JMP
+
+(GETINPUT)
+	@24576
+	D=M
+	@WHITE
+	D;JEQ
+	@BLACK
+	0;JMP
+
+(WHITE)
+	@color
+	M=0
+	@LOOP
+	0;JMP
+
+(BLACK)
+	@color
+	M=-1
+	@LOOP
+	0;JMP
+
+(LOOP)
+	@color
+	D=M
+	@pixel
+	A=M
+	M=D
+	D=A+1
+	@pixel
+	M=D
+  @24575
+  D=D-A
+  @RESET
+  D;JEQ
+	@GETINPUT
+	0;JMP
+
+(RESET)
+	@SCREEN
+	D=A
+	@pixel
+	M=D
+	@GETINPUT
+	0;JMP
